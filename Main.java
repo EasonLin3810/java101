9c2ec849433f765b5bcd71edd9fd43c1732e48fe
Eason lin <easonlin@EasondeMacBook-Pro.local>
2024-11-17 16:37:26 -0500

    added readme and main file

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("im a nigger"); 
+    }
+}
+git add .
+git commit -m "added readme and main file"
+git push
